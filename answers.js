1b335da1a5394af0c8718f5e22db86b0261b673e
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-07-31 01:21:44 -0400

    Easy Going answered

--- answers.js
+++ answers.js
@@ -1,8 +1,5 @@
-// Part II: JavaScript Reps
-// Easy Going
-// Write a for loop that will log the numbers 1 through 20
-
-for(let i = 1; i<=20; i++) {
+//Write a for loop that will log the numbers 1 through 20.
+for (let i = 1; i <= 20; i++) {
     console.log(i);
 
 }
